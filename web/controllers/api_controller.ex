0a2b303180912ecd6eae627cdcccc56e330e9e3c
ovidiubite <ovidiu_bite10@hotmail.com>
2016-11-05 14:40:19 +0200

    broadcasting message [#yolo]

--- web/controllers/api_controller.ex
+++ web/controllers/api_controller.ex
@@ -6,8 +6,31 @@ defmodule Web.APIController do
   def git_hook(conn, _params) do
     params = _params
     Logger.info "Json from github: #{inspect(params)}"
-    Logger.debug "#{inspect(params)}"
-    Logger.info "#{inspect(params)}"
+
+    if Map.has_key?(params, "deployment") do
+
+      creator_name = if Map.has_key?(params, "creator") && Map.has_key?(params, "login") do
+        params["deployment"]["creator"]["login"]
+      else
+        "unknown user"
+      end
+
+      aplication_name = if Map.has_key?(params, "environment") do
+        params["deployment"]["environment"]
+      else
+        "unknown app"
+      end
+
+      task = if Map.has_key?(params, "task") do
+        params["deployment"]["task"]
+      else
+        "unknown task"
+      end
+
+      message = "#{String.capitalize(creator_name)} made a #{task} on #{String.capitalize(aplication_name)}"
+      Web.Endpoint.broadcast("room:lobby", "new_msg", %{"body" => message})
+    end
+
     conn
     |> put_status(:created)
     |> json(params)
