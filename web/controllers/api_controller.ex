65aa1b0703421190a63b02c1a55e19180cfba25e
ovidiubite <ovidiu_bite10@hotmail.com>
2016-11-05 13:51:41 +0200

    logger test

--- web/controllers/api_controller.ex
+++ web/controllers/api_controller.ex
@@ -5,8 +5,9 @@ defmodule Web.APIController do
   # post for git webhook
   def git_hook(conn, _params) do
     params = _params
-    Logger.info "Json from github:"
+    Logger.info "Json from github: #{inspect(params)}"
     Logger.debug "#{inspect(params)}"
+    Logger.info "#{inspect(params)}"
     conn
     |> put_status(:created)
     |> json(params)
